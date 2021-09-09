1ede4b59c7300a9d08c369036bde7ae4d866dcca
Mihocsa Szilard <mihocsa48@gmail.com>
2021-09-09 17:07:11 +0200

    increase chat page text contrast

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 export default class Chat extends Component {
   constructor(props) {
@@ -21,11 +21,20 @@ export default class Chat extends Component {
           // if the user doesn't type a name the welcome message changes
         }
         {name ? (
-          <Text>Hello, {name}!</Text>
+          <Text style={styles.welcomeText}>Hello, {name}!</Text>
         ) : (
-          <Text>You forgot to type your name!</Text>
+          <Text style={styles.welcomeText}>You forgot to type your name!</Text>
         )}
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  welcomeText: {
+    fontSize: 16,
+    color: "#fff",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+    textShadowColor: "#000",
+  },
+});
