9c6698ca87ae34e001ac558641158a60acbc5cb5
Mihocsa Szilard <mihocsa48@gmail.com>
2021-09-17 16:46:00 +0200

    chatbot + store in firebase works

--- components/Chat.js
+++ components/Chat.js
@@ -10,6 +10,19 @@ import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { Dialogflow_V2 } from "react-native-dialogflow";
 
 import { dialogflowConfig } from "./../env";
+import { QuerySnapshot } from "@firebase/firestore";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+//cpnfigure Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAqcDCs-wSluDnUiPVyxf5uc_9QHg7kBxU",
+  authDomain: "chat-app-4ae7f.firebaseapp.com",
+  projectId: "chat-app-4ae7f",
+  storageBucket: "chat-app-4ae7f.appspot.com",
+  messagingSenderId: "706220138883",
+  appId: "1:706220138883:web:46b85201b3680c6a9756b6",
+};
 
 const ChatBot = {
   _id: 2,
@@ -21,8 +34,17 @@ export default class Chat extends Component {
   constructor() {
     super();
     this.state = {
+      isTyping: false,
       messages: [],
     };
+
+    //if no firebase
+    if (!firebase.apps.length) {
+      //initialize
+      firebase.initializeApp(firebaseConfig);
+    }
+    //reference to the chatME collection
+    this.referenceMessages = firebase.firestore().collection("messages");
   }
 
   componentDidMount() {
@@ -31,40 +53,7 @@ export default class Chat extends Component {
     //display the name in header as screen title in Chat
     this.props.navigation.setOptions({ title: name });
     this.setState({
-      //mock-data
-      messages: [
-        {
-          _id: 4,
-          text: "Hello!",
-          createdAt: new Date(),
-          user: {
-            _id: 1,
-            name: name,
-          },
-        },
-        {
-          _id: 3,
-          text: name + " has joined the chat!",
-          createdAt: new Date(),
-          system: true,
-        },
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "ChatBot",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 0,
-          text: "ChatBot has joined the chat!",
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
+      isTyping: false,
     });
 
     Dialogflow_V2.setConfiguration(
@@ -73,7 +62,38 @@ export default class Chat extends Component {
       Dialogflow_V2.LANG_ENGLISH_US,
       dialogflowConfig.project_id
     );
+
+    this.referenceMessages = firebase.firestore().collection("messages");
+    if (this.referenceMessages) {
+      this.unsubscribe = this.referenceMessages.onSnapshot(
+        this.onCollectionUpdate
+      );
+    } else {
+      (error) => console.log(error);
+    }
   }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (QuerySnapshot) => {
+    const messages = [];
+    //go through each document
+    QuerySnapshot.forEach((doc) => {
+      //get the queryDocumentSnapshot's data
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    this.setState({
+      messages: messages.sort((a, b) => Date.parse(b._id) - Date.parse(a._id)),
+    });
+  };
 
   //changing the text bubble color
   renderBubble(props) {
@@ -95,7 +115,13 @@ export default class Chat extends Component {
     );
   }
 
-  hadleGoogleResponse(result) {
+  setIsTyping = () => {
+    this.setState({
+      isTyping: !this.state.isTyping,
+    });
+  };
+
+  handleGoogleResponse(result) {
     let text = result.queryResult.fulfillmentMessages[0].text.text[0];
 
     this.sendBotResponse(text);
@@ -103,45 +129,57 @@ export default class Chat extends Component {
 
   sendBotResponse(text) {
     let msg = {
-      _id: this.state.messages.length + 1,
+      _id: this.state.messages.length + 2,
       text,
       createdAt: new Date(),
       user: ChatBot,
     };
-
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, [msg]),
-    }));
+    this.referenceMessages.add(msg);
   }
 
   //add new messages to 'message' array
   onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessages();
+      }
+    );
 
     let message = messages[0].text;
 
     Dialogflow_V2.requestQuery(
       message,
-      (result) => this.hadleGoogleResponse(result),
+      (result) => this.handleGoogleResponse(result),
       (error) => console.log(error)
       //possible unhandled promise rejection on real ios device (iphone8)
     );
   }
 
-  onQuickReply(quickReply) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, quickReply),
-    }));
+  // onQuickReply(quickReply) {
+  //   this.setState((previousState) => ({
+  //     messages: GiftedChat.append(previousState.messages, quickReply),
+  //   }));
 
-    let message = quickReply[0].value;
+  //   let message = quickReply[0].value;
 
-    Dialogflow_V2.requestQuery(
-      message,
-      (result) => this.hadleGoogleResponse(result),
-      (error) => console.log(error)
-    );
+  //   Dialogflow_V2.requestQuery(
+  //     message,
+  //     (result) => this.handleGoogleResponse(result),
+  //     (error) => console.log(error)
+  //   );
+  // }
+
+  addMessages(message) {
+    message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: this.state.messages.length + 1,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
   }
 
   render() {
@@ -151,9 +189,13 @@ export default class Chat extends Component {
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(message) => this.onSend(message)}
+          renderUsernameOnMessage={true}
+          isTyping={this.state.isTyping}
           onQuickReply={(quickReply) => this.onQuickReply(quickReply)}
           user={{
             _id: 1,
+            name: this.props.route.params.name,
+            avatar: "https://placeimg.com/140/140/any",
           }}
         />
         {/* fix for older Android devices where the input field is hidden beneath the keyboard. */}
